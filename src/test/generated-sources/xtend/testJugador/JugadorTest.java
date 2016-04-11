3679132b2b71583ef03b9d367f1d9a5e513b717b
presaf <federicopresa@yahoo.com.ar>
2016-04-10 23:35:42 -0300

    Se testeó la clase jugador!

--- src/test/generated-sources/xtend/testJugador/JugadorTest.java
+++ src/test/generated-sources/xtend/testJugador/JugadorTest.java
@@ -1,6 +1,11 @@
 package testJugador;
 
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import unq_ciu.gatoEncerrado.Habitacion;
+import unq_ciu.gatoEncerrado.Item;
 import unq_ciu.gatoEncerrado.Jugador;
 
 @SuppressWarnings("all")
@@ -9,6 +14,7 @@ public class JugadorTest {
   
   private Habitacion habitacion;
   
+  @Before
   public void setUp() {
     Habitacion _habitacion = new Habitacion("Living", false, false);
     this.habitacion = _habitacion;
@@ -16,21 +22,109 @@ public class JugadorTest {
     this.jugador = _jugador;
   }
   
+  @Test
   public void testAbandonar() {
+    int resultadoEsperado1 = 0;
+    int resultadoEsperado2 = 1;
+    int resultadoEsperado3 = 2;
+    int _abandonados = this.jugador.getAbandonados();
+    Assert.assertEquals(resultadoEsperado1, _abandonados);
+    this.jugador.abandonar();
+    int _abandonados_1 = this.jugador.getAbandonados();
+    Assert.assertEquals(resultadoEsperado2, _abandonados_1);
+    this.jugador.abandonar();
+    int _abandonados_2 = this.jugador.getAbandonados();
+    Assert.assertEquals(resultadoEsperado3, _abandonados_2);
   }
   
+  @Test
   public void testVerEstadisticas() {
   }
   
+  @Test
   public void testPuedoAgregar() {
+    Item pala1 = new Item("pala1");
+    Item pala2 = new Item("pala2");
+    Item pala3 = new Item("pala3");
+    Item pala4 = new Item("pala4");
+    Item pala5 = new Item("pala5");
+    Item llave1 = new Item("llave1");
+    Item llave2 = new Item("llave2");
+    Item llave3 = new Item("llave3");
+    Item llave4 = new Item("llave4");
+    Item llave5 = new Item("llave5");
+    Item piedra1 = new Item("piedra1");
+    Item piedra2 = new Item("piedra2");
+    Item piedra3 = new Item("piedra3");
+    Item piedra4 = new Item("piedra4");
+    Item piedra5 = new Item("piedra5");
+    boolean _puedeAgregar = this.jugador.puedeAgregar();
+    Assert.assertEquals(Boolean.valueOf(true), Boolean.valueOf(_puedeAgregar));
+    this.jugador.agregarAlInventario(pala1);
+    this.jugador.agregarAlInventario(pala2);
+    this.jugador.agregarAlInventario(pala3);
+    this.jugador.agregarAlInventario(pala4);
+    this.jugador.agregarAlInventario(pala5);
+    this.jugador.agregarAlInventario(llave1);
+    this.jugador.agregarAlInventario(llave2);
+    this.jugador.agregarAlInventario(llave3);
+    this.jugador.agregarAlInventario(llave4);
+    this.jugador.agregarAlInventario(llave5);
+    this.jugador.agregarAlInventario(piedra1);
+    this.jugador.agregarAlInventario(piedra2);
+    this.jugador.agregarAlInventario(piedra3);
+    this.jugador.agregarAlInventario(piedra4);
+    this.jugador.agregarAlInventario(piedra5);
+    boolean _puedeAgregar_1 = this.jugador.puedeAgregar();
+    Assert.assertEquals(Boolean.valueOf(false), Boolean.valueOf(_puedeAgregar_1));
   }
   
+  @Test
   public void testAgregarAlInventario() {
+    Item clavos = new Item("Clavos");
+    int resultadoEsperado1 = 0;
+    int resultadoEsperado2 = 1;
+    List<Item> _inventario = this.jugador.getInventario();
+    int _size = _inventario.size();
+    Assert.assertEquals(resultadoEsperado1, _size);
+    List<Item> _inventario_1 = this.jugador.getInventario();
+    boolean _contains = _inventario_1.contains(clavos);
+    Assert.assertFalse(_contains);
+    this.jugador.agregarAlInventario(clavos);
+    List<Item> _inventario_2 = this.jugador.getInventario();
+    int _size_1 = _inventario_2.size();
+    Assert.assertEquals(resultadoEsperado2, _size_1);
+    List<Item> _inventario_3 = this.jugador.getInventario();
+    boolean _contains_1 = _inventario_3.contains(clavos);
+    Assert.assertTrue(_contains_1);
   }
   
+  @Test
   public void testQuitarDelInventario() {
+    Item llave = new Item("llave");
+    int resultadoEsperado1 = 1;
+    int resultadoEsperado2 = 0;
+    this.jugador.agregarAlInventario(llave);
+    List<Item> _inventario = this.jugador.getInventario();
+    Assert.assertEquals(Integer.valueOf(resultadoEsperado1), _inventario);
+    List<Item> _inventario_1 = this.jugador.getInventario();
+    boolean _contains = _inventario_1.contains(llave);
+    Assert.assertTrue(_contains);
+    this.jugador.quitarDelInventario(llave);
+    List<Item> _inventario_2 = this.jugador.getInventario();
+    Assert.assertEquals(Integer.valueOf(resultadoEsperado2), _inventario_2);
+    List<Item> _inventario_3 = this.jugador.getInventario();
+    boolean _contains_1 = _inventario_3.contains(llave);
+    Assert.assertTrue(_contains_1);
   }
   
+  @Test
   public void testTiene() {
+    Item piedra = new Item("piedra");
+    boolean _tiene = this.jugador.tiene(piedra);
+    Assert.assertFalse(_tiene);
+    this.jugador.agregarAlInventario(piedra);
+    boolean _tiene_1 = this.jugador.tiene(piedra);
+    Assert.assertTrue(_tiene_1);
   }
 }
--- src/test/java/testJugador/JugadorTest.xtend
+++ src/test/java/testJugador/JugadorTest.xtend
@@ -2,40 +2,132 @@ package testJugador
 
 import unq_ciu.gatoEncerrado.Jugador
 import unq_ciu.gatoEncerrado.Habitacion
+import org.junit.Test
+import unq_ciu.gatoEncerrado.Item
+import org.junit.Before
+import static org.junit.Assert.*;
 
 class JugadorTest {
 	var Jugador jugador
 	var Habitacion habitacion
-
+	
+	@Before
 	def void setUp() {
-
+		
 		//Establezco el escenario para testear
 		habitacion = new Habitacion("Living", false, false)
 		jugador = new Jugador("Pepe", habitacion)
-	}
-
+		}
+	
+	@Test
 	def void testAbandonar() {
-		//Falta testear
+		var resultadoEsperado1 = 0
+		var resultadoEsperado2 = 1
+		var resultadoEsperado3 = 2
+		
+		assertEquals(resultadoEsperado1, jugador.abandonados)
+		
+		jugador.abandonar()
+		
+		assertEquals(resultadoEsperado2, jugador.abandonados)
+		
+		jugador.abandonar()
+		
+		assertEquals(resultadoEsperado3, jugador.abandonados)
 	}
-
+	
+	
+	@Test
 	def void testVerEstadisticas() {
-		//Falta testear
+		//Hay que ver si esto hay que testearlo
 	}
-
+	
+	
+	@Test
 	def void testPuedoAgregar() {
-		//Falta testear
+		var Item pala1 = new Item("pala1")
+		var Item pala2 = new Item("pala2")
+		var Item pala3 = new Item("pala3")
+		var Item pala4 = new Item("pala4")
+		var Item pala5 = new Item("pala5")
+		var Item llave1 = new Item("llave1")
+		var Item llave2 = new Item("llave2")
+		var Item llave3 = new Item("llave3")
+		var Item llave4 = new Item("llave4")
+		var Item llave5 = new Item("llave5")
+		var Item piedra1 = new Item("piedra1")
+		var Item piedra2 = new Item("piedra2")
+		var Item piedra3 = new Item("piedra3")
+		var Item piedra4 = new Item("piedra4")
+		var Item piedra5 = new Item("piedra5")
+			
+		assertEquals(true, jugador.puedeAgregar())
+		
+		jugador.agregarAlInventario(pala1)
+		jugador.agregarAlInventario(pala2)
+		jugador.agregarAlInventario(pala3)
+		jugador.agregarAlInventario(pala4)
+		jugador.agregarAlInventario(pala5)
+		jugador.agregarAlInventario(llave1)
+		jugador.agregarAlInventario(llave2)
+		jugador.agregarAlInventario(llave3)
+		jugador.agregarAlInventario(llave4)
+		jugador.agregarAlInventario(llave5)
+		jugador.agregarAlInventario(piedra1)
+		jugador.agregarAlInventario(piedra2)
+		jugador.agregarAlInventario(piedra3)
+		jugador.agregarAlInventario(piedra4)
+		jugador.agregarAlInventario(piedra5)
+		
+		assertEquals(false, jugador.puedeAgregar())
 	}
-
+	
+	
+	@Test
 	def void testAgregarAlInventario() {
-		//Falta testear
+		var clavos = new Item("Clavos")
+		
+		var resultadoEsperado1 = 0
+		var resultadoEsperado2 = 1
+		
+		assertEquals(resultadoEsperado1, jugador.inventario.size)
+		assertFalse(jugador.inventario.contains(clavos))
+		
+		jugador.agregarAlInventario(clavos)
+		
+		assertEquals(resultadoEsperado2, jugador.inventario.size)
+		assertTrue(jugador.inventario.contains(clavos))
 	}
-
+	
+	
+	@Test
 	def void testQuitarDelInventario() {
-		//Falta testear
+		var llave = new Item("llave")
+		
+		var resultadoEsperado1 = 1
+		var resultadoEsperado2 = 0
+		
+		jugador.agregarAlInventario(llave)
+		
+		assertEquals(resultadoEsperado1, jugador.inventario)
+		assertTrue(jugador.inventario.contains(llave))
+		
+		jugador.quitarDelInventario(llave)
+		
+		assertEquals(resultadoEsperado2, jugador.inventario)
+		assertTrue(jugador.inventario.contains(llave))
 	}
-
+	
+	
+	@Test
 	def void testTiene() {
-		//Falta testear
+		var piedra = new Item("piedra")
+		
+		assertFalse(jugador.tiene(piedra))
+			
+		jugador.agregarAlInventario(piedra)
+		
+		assertTrue(jugador.tiene(piedra))
 	}
-
+	
 }
