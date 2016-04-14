efd980bac474639eb89bc6b067616230c95a5407
Fpresa <Fpresa@eaa-sis08.europ-assistance.local>
2016-04-14 11:49:15 -0300

    Se trabajó sobre las ventanas de consecuencias!

--- src/main/generated-sources/xtend/unq_ciu/gatoEncerrado/Consecuencias/AgregarAccionDeAgarrarUnElementoConsecuenciaAppModel.java
+++ src/main/generated-sources/xtend/unq_ciu/gatoEncerrado/Consecuencias/AgregarAccionDeAgarrarUnElementoConsecuenciaAppModel.java
@@ -1,69 +1,41 @@
 package unq_ciu.gatoEncerrado.Consecuencias;
 
 import com.google.common.base.Objects;
-import java.util.ArrayList;
-import java.util.List;
 import org.eclipse.xtend.lib.annotations.Accessors;
-import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import org.eclipse.xtext.xbase.lib.Pure;
 import org.uqbar.commons.model.UserException;
 import org.uqbar.commons.utils.Observable;
 import unq_ciu.gatoEncerrado.Habitacion;
 import unq_ciu.gatoEncerrado.Item;
-import unq_ciu.gatoEncerrado.Laberinto;
 import unq_ciu.gatoEncerrado.acciones.Agarrar;
-import unq_ciu.gatoEncerrado.acciones.Usar;
 
 @Accessors
 @Observable
 @SuppressWarnings("all")
 public class AgregarAccionDeAgarrarUnElementoConsecuenciaAppModel {
-  private Laberinto laberinto;
-  
   private Habitacion habitacion;
   
-  private Habitacion habitacionSeleccionada;
+  private Item itemDisponibleComoConsecuencia;
   
-  private Item item;
+  private Item itemUtilizado;
   
-  public AgregarAccionDeAgarrarUnElementoConsecuenciaAppModel(final Laberinto lab, final Habitacion hab, final Item item) {
-    this.laberinto = lab;
-    this.habitacion = hab;
-    this.item = item;
+  public AgregarAccionDeAgarrarUnElementoConsecuenciaAppModel(final Habitacion habitacion) {
+    this.habitacion = habitacion;
+    Item _item = new Item();
+    this.itemDisponibleComoConsecuencia = _item;
   }
   
-  public boolean agregarConsecuencia() {
+  public boolean agregarAcccionAgarrar() {
     boolean _xifexpression = false;
-    boolean _equals = Objects.equal(this.habitacionSeleccionada, null);
+    String _nombre = this.itemDisponibleComoConsecuencia.getNombre();
+    boolean _equals = Objects.equal(_nombre, null);
     if (_equals) {
-      throw new UserException("Por favor seleccione una habitación.");
+      throw new UserException("Por favor ingrese un nombre para el elemento.");
     } else {
-      Agarrar _agarrar = new Agarrar(this.item);
-      Usar _usar = new Usar(this.item, _agarrar);
-      _xifexpression = this.habitacion.agregarAccion(_usar);
+      Agarrar _agarrar = new Agarrar(this.itemUtilizado);
+      _xifexpression = this.habitacion.agregarAccion(_agarrar);
     }
     return _xifexpression;
-  }
-  
-  public List<Habitacion> getHabitacionesDisponibles() {
-    final ArrayList<Habitacion> lista = CollectionLiterals.<Habitacion>newArrayList();
-    List<Habitacion> _habitaciones = this.laberinto.getHabitaciones();
-    for (final Habitacion h : _habitaciones) {
-      boolean _notEquals = (!Objects.equal(h, this.habitacion));
-      if (_notEquals) {
-        lista.add(h);
-      }
-    }
-    return lista;
-  }
-  
-  @Pure
-  public Laberinto getLaberinto() {
-    return this.laberinto;
-  }
-  
-  public void setLaberinto(final Laberinto laberinto) {
-    this.laberinto = laberinto;
   }
   
   @Pure
@@ -76,20 +48,20 @@ public class AgregarAccionDeAgarrarUnElementoConsecuenciaAppModel {
   }
   
   @Pure
-  public Habitacion getHabitacionSeleccionada() {
-    return this.habitacionSeleccionada;
+  public Item getItemDisponibleComoConsecuencia() {
+    return this.itemDisponibleComoConsecuencia;
   }
   
-  public void setHabitacionSeleccionada(final Habitacion habitacionSeleccionada) {
-    this.habitacionSeleccionada = habitacionSeleccionada;
+  public void setItemDisponibleComoConsecuencia(final Item itemDisponibleComoConsecuencia) {
+    this.itemDisponibleComoConsecuencia = itemDisponibleComoConsecuencia;
   }
   
   @Pure
-  public Item getItem() {
-    return this.item;
+  public Item getItemUtilizado() {
+    return this.itemUtilizado;
   }
   
-  public void setItem(final Item item) {
-    this.item = item;
+  public void setItemUtilizado(final Item itemUtilizado) {
+    this.itemUtilizado = itemUtilizado;
   }
 }
--- src/main/java/unq_ciu/gatoEncerrado/Consecuencias/AgregarAccionDeAgarrarUnElementoConsecuenciaAppModel.xtend
+++ src/main/java/unq_ciu/gatoEncerrado/Consecuencias/AgregarAccionDeAgarrarUnElementoConsecuenciaAppModel.xtend
@@ -1,12 +1,9 @@
 package unq_ciu.gatoEncerrado.Consecuencias
 
-import unq_ciu.gatoEncerrado.Laberinto
 import unq_ciu.gatoEncerrado.Habitacion
 import unq_ciu.gatoEncerrado.Item
 import org.uqbar.commons.model.UserException
 import unq_ciu.gatoEncerrado.acciones.Agarrar
-import unq_ciu.gatoEncerrado.acciones.Usar
-import java.util.List
 import org.eclipse.xtend.lib.annotations.Accessors
 import org.uqbar.commons.utils.Observable
 
@@ -14,34 +11,20 @@ import org.uqbar.commons.utils.Observable
 @Observable
 class AgregarAccionDeAgarrarUnElementoConsecuenciaAppModel {
 
-	Laberinto laberinto
 	Habitacion habitacion
-	Habitacion habitacionSeleccionada
-	Item item
+	Item itemDisponibleComoConsecuencia
+	Item itemUtilizado
 
-	new(Laberinto lab, Habitacion hab, Item item) {
-		this.laberinto = lab
-		this.habitacion = hab
-		this.item = item
+	new(Habitacion habitacion) {
+		this.habitacion = habitacion
+		this.itemDisponibleComoConsecuencia = new Item()
 	}
 
-	def agregarConsecuencia() {
-
-		if (habitacionSeleccionada == null) {
-			throw new UserException("Por favor seleccione una habitación.")
+	def agregarAcccionAgarrar() {
+		if (this.itemDisponibleComoConsecuencia.nombre == null) {
+			throw new UserException("Por favor ingrese un nombre para el elemento.")
 		} else {
-			habitacion.agregarAccion(new Usar(item, new Agarrar(item)))
+			habitacion.agregarAccion(new Agarrar(this.itemUtilizado))
 		}
-	}
-
-	def List<Habitacion> getHabitacionesDisponibles() {
-		val lista = newArrayList()
-		for (Habitacion h : laberinto.habitaciones) {
-			if (h != habitacion) {
-				lista.add(h)
-			}
-		}
-		return lista
-
 	}
 }
